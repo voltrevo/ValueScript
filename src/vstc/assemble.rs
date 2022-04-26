e47690b83febfb8de3eaf11810ddbfc88aa28cf4
Andrew Morris <voltrevo@gmail.com>
2022-04-26 11:11:53 +1000

    Implement help for vstc assemble

--- src/vstc/assemble.rs
+++ src/vstc/assemble.rs
@@ -1,3 +1,24 @@
+use std::process::exit;
+
 pub fn command(args: &Vec<String>) {
-  println!("assemble command test {}", args.len())
+  if args.len() != 3 {
+    println!("ERROR: Unrecognized command\n");
+    show_help();
+    exit(1);
+  }
+
+  if args[2] == "-h" || args[2] == "--help" {
+    show_help();
+    return;
+  }
+
+  println!("TODO: assemble file {}", args[2]);
+}
+
+fn show_help() {
+  println!("vstc assemble");
+  println!("Convert ValueScript assembly to bytecode");
+  println!("");
+  println!("USAGE:");
+  println!("    vstc assemble <file>");
 }
