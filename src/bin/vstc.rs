ffc5f4709d387868f7475e23e9d621809da1023e
Andrew Morris <voltrevo@gmail.com>
2022-04-26 10:23:11 +1000

    Implement vstc help

--- src/bin/vstc.rs
+++ src/bin/vstc.rs
@@ -1,3 +1,47 @@
+use std::env;
+
 fn main() {
-    println!("vstc stub");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() == 2 && (
+        args[1] == "-h" ||
+        args[1] == "--help" ||
+        args[1] == "help"
+     ) {
+        show_help();
+        return;
+    }
+}
+
+fn show_help() {
+    println!("ValueScript toolchain 0.1.0");
+    println!("");
+    println!("USAGE:");
+    println!("    vstc [OPTIONS] [SUBCOMMAND]");
+    println!("");
+    println!("OPTIONS:");
+    println!("    -h, --help");
+    println!("            Print help information");
+    println!("");
+    println!("    -V, --version");
+    println!("            Print version information");
+    println!("");
+    println!("SUBCOMMANDS:");
+    println!("    compile");
+    println!("            Compile an entry point");
+    println!("");
+    println!("    assemble");
+    println!("            Convert assembly to bytecode");
+    println!("");
+    println!("    disassemble");
+    println!("            Convert bytecode to assembly");
+    println!("");
+    println!("    run");
+    println!("            Run a program");
+    println!("");
+    println!("    repl");
+    println!("            Read Eval Print Loop");
+    println!("");
+    println!("    host");
+    println!("            Start database server");
 }
