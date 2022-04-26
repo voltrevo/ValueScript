6d2b4e116fc4c9d02fac29210ac98c6c130664d5
Andrew Morris <voltrevo@gmail.com>
2022-04-26 11:35:30 +1000

    Set up assemble stub

--- src/vstc/assemble.rs
+++ src/vstc/assemble.rs
@@ -12,7 +12,22 @@ pub fn command(args: &Vec<String>) {
     return;
   }
 
-  println!("TODO: assemble file {}", args[2]);
+  let read_result = std::fs::read_to_string(&args[2]);
+
+  if read_result.is_err() {
+    println!("Failed to read file {}", args[2]);
+    return;
+  }
+
+  let content = read_result.expect("");
+  let output_filename = "out.vsb";
+
+  let write_result = std::fs::write(output_filename, assemble(&content));
+
+  if write_result.is_err() {
+    println!("Failed to write file {}", output_filename);
+    std::process::exit(1);
+  }
 }
 
 fn show_help() {
@@ -21,4 +36,12 @@ fn show_help() {
   println!("");
   println!("USAGE:");
   println!("    vstc assemble <file>");
+}
+
+fn assemble(content: &String) -> std::vec::Vec<u8> {
+  let mut output: Vec<u8> = Vec::new();
+
+  // TODO: Assemble content into output
+  
+  return output;
 }
