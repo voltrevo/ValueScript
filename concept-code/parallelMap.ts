ac295889f59203d93deab550e26cf6dcf9035c9d
Andrew Morris <voltrevo@gmail.com>
2022-05-01 17:58:36 +1000

    Add concurrencyLimit to parallelMap

--- concept-code/parallelMap.ts
+++ concept-code/parallelMap.ts
@@ -3,8 +3,23 @@ import { thread } from 'value-script';
 export default function parallelMap<Value, MappedValue>(
   values: Value[],
   mapper: (value: Value) => MappedValue,
+  concurrencyLimit = 10,
 ) {
-  return values
-    .map(v => thread(() => mapper(v)))
-    .map(t => t());
+  let poolSize = Math.min(values.length, concurrencyLimit);
+
+  const pool = values
+    .slice(0, poolSize)
+    .map(v => thread(() => mapper(v)));
+
+  let results: MappedValue[] = [];
+
+  for (let i = 0; i < values.length; i++) {
+    results[i] = pool[i % poolSize]();
+
+    if (i + poolSize < values.length) {
+      pool[i % poolSize] = thread(() => mapper(values[i + poolSize]));
+    }
+  }
+
+  return results;
 }
