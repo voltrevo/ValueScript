67f530a348beaa2c1b6fd9042cfd72a7549e2a0a
Andrew Morris <voltrevo@gmail.com>
2022-05-31 10:46:48 +1000

    projEuler9.ts

---
+++ inputs/passing/projEuler9.ts
@@ -0,0 +1,15 @@
+export default function main() {
+  for (let a = 1; a <= 1000; a++) {
+    for (let b = a; b <= 1000; b++) {
+      const c = 1000 - a - b;
+
+      if (c <= b) {
+        break;
+      }
+
+      if (a * a + b * b === c * c) {
+        return a * b * c;
+      }
+    }
+  }
+}
