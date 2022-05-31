cf1399d4cb7b5c5cacc0c0612a9ea1c2b2f649ba
Andrew Morris <voltrevo@gmail.com>
2022-05-31 10:31:19 +1000

    projEuler6.ts

---
+++ inputs/passing/projEuler6.ts
@@ -0,0 +1,11 @@
+export default function main() {
+  return squareOfSum(100) - sumOfSquares(100);
+}
+
+function sumOfSquares(n: number) {
+  return n * (n + 1) * (2 * n + 1) / 6;
+}
+
+function squareOfSum(n: number) {
+  return (n * (n + 1) / 2) ** 2;
+}
