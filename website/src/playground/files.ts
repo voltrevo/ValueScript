6d88e0b7e4abb8340a4a79ee2388ddfe645b9bf3
Andrew Morris <voltrevo@gmail.com>
2022-12-14 15:53:25 +1100

    Use class version of counter example

--- website/src/playground/files.ts
+++ website/src/playground/files.ts
@@ -93,19 +93,22 @@ const files: Record<string, string | nil> = {
   `),
 
   "examples/counter.ts": blockTrim(`
-    export default function() {
-      let c = Counter();
-    
+    export default function main() {
+      let c = new Counter();
+
       return [c.get(), c.get(), c.get()];
     }
-    
-    function Counter() {
-      return {
-        next: 1,
-        get: function() {
-          return this.next++;
-        },
-      };
+
+    class Counter {
+      next: number;
+
+      constructor() {
+        this.next = 1;
+      }
+
+      get() {
+        return this.next++;
+      }
     }
   `),
 
@@ -168,37 +171,37 @@ const files: Record<string, string | nil> = {
   "examples/mergeSort.ts": blockTrim(`
     export default function main() {
       const x = [7, 18, 9, 11, 16, 3, 8, 2, 5, 4, 6, 14, 15, 17, 10, 12, 1, 13];
-    
+
       return mergeSort(x, (a, b) => a - b);
     }
-    
+
     function mergeSort<T>(vals: T[], cmp: (a: T, b: T) => number): T[] {
       const len = vals.length;
-    
+
       if (len <= 1) {
         return vals;
       }
-    
+
       if (len === 2) {
         if (cmp(vals[0], vals[1]) > 0) {
           return [vals[1], vals[0]];
         }
-    
+
         return vals;
       }
-    
+
       const mid = vals.length / 2;
-    
+
       const leftSorted = mergeSort(vals.slice(0, mid), cmp);
       const rightSorted = mergeSort(vals.slice(mid), cmp);
-    
+
       let res: T[] = [];
-    
+
       let left = 0;
       const leftLen = leftSorted.length;
       let right = 0;
       const rightLen = rightSorted.length;
-    
+
       while (left < leftLen && right < rightLen) {
         if (cmp(leftSorted[left], rightSorted[right]) <= 0) {
           res.push(leftSorted[left++]);
@@ -206,17 +209,17 @@ const files: Record<string, string | nil> = {
           res.push(rightSorted[right++]);
         }
       }
-    
+
       while (left < leftLen) {
         res.push(leftSorted[left++]);
       }
-    
+
       while (right < rightLen) {
         res.push(rightSorted[right++]);
       }
-    
+
       return res;
-    }  
+    }
   `),
 };
 
