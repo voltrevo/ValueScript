7ea97ad22b1263012dce350c8580b03e47de8ea8
Andrew Morris <voltrevo@gmail.com>
2023-03-02 18:39:23 +1100

    More test updates

--- inputs/failing/assignmentEvalOrder.ts
+++ inputs/failing/assignmentEvalOrder.ts
@@ -1,5 +1,10 @@
 // test_output! {"right":"right"}
 // (This is wrong.)
+// Note: The reason for the error (at the time of writing) is not actually
+// evaluation order but the use of the *register* %key for the left side. `key`
+// is correctly 'evaluated' first, but it doesn't get its own register; it's
+// just %key.
+// For reference, assignmentEvalOrder2.ts tests the actual order of evaluation.
 
 export default function main() {
   const x = {} as any;
---
+++ inputs/passing/assignmentEvalOrder2.ts
@@ -0,0 +1,10 @@
+// test_output! ["a","b"]
+
+export default function main() {
+  let log = [];
+  const x = {} as any;
+
+  x[log.push('a')] = log.push('b');
+
+  return log;
+}
