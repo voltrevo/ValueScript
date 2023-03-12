66a09bf11845de8f93ac7ffe06b23611e4e16c59
Andrew Morris <voltrevo@gmail.com>
2023-03-13 10:34:48 +1100

    charAt test

---
+++ inputs/passing/stringMethods/charAt.ts
@@ -0,0 +1,18 @@
+// test_output! [["","f","o","o",""],["f","f","o"]]
+
+export default function() {
+  return [
+    [
+      "foo".charAt(-1),
+      "foo".charAt(0),
+      "foo".charAt(1),
+      "foo".charAt(2),
+      "foo".charAt(3),
+    ],
+    [
+      "foo".charAt(0/0), // (0/0 is NaN, keyword not implemented yet)
+      "foo".charAt(-0.9),
+      "foo".charAt([1]),
+    ],
+  ];
+}
