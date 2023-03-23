ba68ef697a58acff80bb942f6bb910b25293e6ac
Andrew Morris <voltrevo@gmail.com>
2023-03-23 15:48:33 +1100

    Test exceptions

---
+++ inputs/passing/exceptions/nestedTry.ts
@@ -0,0 +1,21 @@
+// test_output! ["here","nested boom","boom"]
+
+export default function () {
+  let logs: unknown[] = [];
+
+  try {
+    logs.push("here");
+
+    try {
+      throw new Error("nested boom");
+    } catch (error) {
+      logs.push(error.message);
+    }
+
+    throw new Error("boom");
+  } catch (error) {
+    logs.push(error.message);
+  }
+
+  return logs;
+}
---
+++ inputs/passing/exceptions/tryCatch.ts
@@ -0,0 +1,9 @@
+// test_output! Error{"message":"Something went wrong"}
+
+export default function () {
+  try {
+    throw new Error("Something went wrong");
+  } catch (error) {
+    return error;
+  }
+}
