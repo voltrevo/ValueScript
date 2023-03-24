0f26aadd540717e20d7d9f4d25a6c8058a174a20
Andrew Morris <voltrevo@gmail.com>
2023-03-24 18:36:22 +1100

    Better test, update website

--- website/src/playground/files.ts
+++ website/src/playground/files.ts
@@ -66,6 +66,27 @@ const files: Record<string, string | nil> = {
     // and therefore \`leftBowl\` doesn't change.
   `),
 
+  "tutorial/revertOnCatch.ts": blockTrim(`
+    export default function () {
+      let x = 0;
+
+      try {
+        x++;
+        throw new Error("boom");
+      } catch {}
+    
+      return x;
+      // TypeScript:  1
+      // ValueScript: 0
+    }
+
+    // In ValueScript, a try block is a transaction - it either runs to
+    // completion, or it is reverted. This is impractical in TypeScript,
+    // but in ValueScript all we have to do is snapshot the variables and
+    // restore from them on catch. This works because all mutation is
+    // local - nothing else can be affected.
+  `),
+
   "examples/factorial.ts": blockTrim(`
     export default function main() {
       return factorial(5);
