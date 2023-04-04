90eef3f8269a0cb74ed014c835dfe22932d0dd44
Andrew Morris <voltrevo@gmail.com>
2023-04-04 15:28:06 +1000

    Add failing input

---
+++ inputs/failing/exceptions/snapshotThis.ts
@@ -0,0 +1,20 @@
+// test_output! 1
+// (Should be 0.)
+
+export default function main() {
+  const foo = new Foo();
+  foo.inc();
+
+  return foo.x;
+}
+
+class Foo {
+  x = 0;
+
+  inc() {
+    try {
+      this.x++;
+      throw new Error("boom");
+    } catch {}
+  }
+}
