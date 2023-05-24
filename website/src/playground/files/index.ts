ceb95b97c86f5379d85981449e03bfb95adf3026
Andrew Morris <voltrevo@gmail.com>
2023-05-24 16:04:58 +1000

    Remove console.log

--- website/src/playground/files/index.ts
+++ website/src/playground/files/index.ts
@@ -21,8 +21,6 @@ export const defaultFiles: Record<string, string | nil> = {
   ...raw,
 };
 
-console.log(Object.entries(defaultFiles));
-
 function pick<T, K extends keyof T>(obj: T, keys: K[]): Pick<T, K> {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const result: any = {};
