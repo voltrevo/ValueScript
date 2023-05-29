8f8d840b7109674435d4a99883a98412950b25db
Andrew Morris <voltrevo@gmail.com>
2023-05-29 12:53:10 +1000

    Test manual array iteration

---
+++ inputs/passing/iteration/manualArrayIteration.ts
@@ -0,0 +1,14 @@
+// test_output! [[1,false],[2,false],[3,false],[undefined,true],[undefined,true]]
+
+export default function () {
+  const vals = [1, 2, 3];
+  let iter = vals.values();
+
+  return [
+    iter.next(),
+    iter.next(),
+    iter.next(),
+    iter.next(),
+    iter.next(),
+  ].map(({ value, done }) => [value, done]);
+}
