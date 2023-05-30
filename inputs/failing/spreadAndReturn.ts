ae729a3a295075c3e5f49888aa314e6fdc126c0c
Andrew Morris <voltrevo@gmail.com>
2023-05-30 15:21:43 +1000

    Add failing test

---
+++ inputs/failing/spreadAndReturn.ts
@@ -0,0 +1,16 @@
+// // test_output! {}
+
+export default function () {
+  let x = new X();
+  [...x];
+
+  return x;
+}
+
+class X {
+  constructor() {}
+
+  [Symbol.iterator]() {
+    return { next: () => ({ value: undefined, done: true }) };
+  }
+}
