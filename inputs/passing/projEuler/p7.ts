d10adf83be73f89a8868af068ca248d75f9bb3b9
Andrew Morris <voltrevo@gmail.com>
2023-06-22 20:47:49 +1000

    Fix p7 size (now about 0.7s in release)

--- inputs/passing/projEuler/p7.ts
+++ inputs/passing/projEuler/p7.ts
@@ -1,5 +1,7 @@
+//! test_output_slow(104743)
+
 import { Range_primes } from "../helpers/range.ts";
 
 export default function main() {
-  return Range_primes().at(1_000);
+  return Range_primes().at(10_000);
 }
