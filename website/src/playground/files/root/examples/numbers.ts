61b1f6471e14e3c74afc4ec77ea7158d2669e371
Andrew Morris <voltrevo@gmail.com>
2023-06-23 10:24:30 +1000

    range -> numbers

--- website/src/playground/files/root/examples/numbers.ts
+++ website/src/playground/files/root/examples/numbers.ts
@@ -1,8 +1,8 @@
 export default function main() {
-  return [...range(0, 10)];
+  return [...numbers(0, 10)];
 }
 
-function* range(start: number, end: number) {
+function* numbers(start: number, end: number) {
   for (let i = start; i < end; i++) {
     yield i;
   }
