7a7ba8692117e96cb573b1c94059f3ff9fe25831
Andrew Morris <voltrevo@gmail.com>
2023-07-01 09:14:51 +1000

    Add failing test

---
+++ inputs/failing/copyCounting/exception.ts
@@ -0,0 +1,19 @@
+//! test_output(3)
+// Should be: 0
+
+/// <reference path="../../../concept-code/vs.d.ts" />
+
+export default function () {
+  try {
+    throwCCEx();
+  } catch (e) {
+    return e.count;
+  }
+}
+
+function throwCCEx() {
+  while (true) {
+    const x = Debug.makeCopyCounter("x");
+    throw x;
+  }
+}
