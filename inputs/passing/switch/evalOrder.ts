6c18165a5eeaa4bdabc11c534ee2ff51a81d6cc8
Andrew Morris <voltrevo@gmail.com>
2023-07-06 11:49:18 +1000

    Formatting

--- inputs/passing/switch/evalOrder.ts
+++ inputs/passing/switch/evalOrder.ts
@@ -1,14 +1,16 @@
 //! test_output(["c1","c2","default"])
 
-export default function foo() {
+export default function () {
   let logs = [];
 
   switch (0) {
     case logs.push("c1"):
       break;
+
     default:
       logs.push("default");
       break;
+
     case logs.push("c2"):
       break;
   }
