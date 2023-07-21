0d59881d82bb579d993263a5a4138d67c8ab8ddb
Andrew Morris <voltrevo@gmail.com>
2023-07-21 17:51:19 +1000

    Document SWC bug

--- inputs/failing/spuriousAssignmentBug.ts
+++ inputs/failing/spuriousAssignmentBug.ts
@@ -1,3 +1,8 @@
+// This is an SWC bug. It appears to be fixed in the latest version, but the latest version also
+// doesn't compile in stable rust. Hopefully this is also fixed in the latest stable version, we
+// just need to figure out what that is.
+// Related: https://github.com/swc-project/cli/issues/218.
+
 export default function main() {
   return [3n < "asdf", 3n >= "asdf"];
 }
