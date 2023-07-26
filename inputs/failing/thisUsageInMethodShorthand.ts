42b8a5ad40531597b865204269cd8bbf4a80a3b6
Andrew Morris <voltrevo@gmail.com>
2023-07-26 10:25:33 +1000

    Add failing test

---
+++ inputs/failing/thisUsageInMethodShorthand.ts
@@ -0,0 +1,14 @@
+export default function main() {
+  let stuff = {
+    foo() {
+      this.bar = "baz";
+      return this.bar;
+    },
+    bar: "bar",
+  };
+
+  return [
+    stuff.foo(),
+    stuff.bar,
+  ];
+}
