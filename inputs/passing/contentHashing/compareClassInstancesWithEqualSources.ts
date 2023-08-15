76cdacfed3faeb7bd279f72fd853cc2bac202e9a
Andrew Morris <voltrevo@gmail.com>
2023-08-15 16:31:42 +1000

    Expand test case

--- inputs/passing/contentHashing/compareClassInstancesWithEqualSources.ts
+++ inputs/passing/contentHashing/compareClassInstancesWithEqualSources.ts
@@ -1,4 +1,4 @@
-//! test_output(true)
+//! test_output([true,true,true,true,true])
 
 // When functions (and therefore classes) have the same source (and references), they should compare
 // equal due to the content hash system.
@@ -7,7 +7,14 @@ export default () => {
   const a = new classes[0](1, 2);
   const b = new classes[1](1, 2);
 
-  return a === b;
+  return [
+    a === b,
+    // classes[0] === classes[1], TODO: Compare actual classes
+    a instanceof classes[0],
+    a instanceof classes[1],
+    b instanceof classes[0],
+    b instanceof classes[1],
+  ];
 };
 
 const classes = [
