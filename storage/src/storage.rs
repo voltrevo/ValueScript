8dc44f0b21aa61bf72331bb2cc9aee2952e743a4
Andrew Morris <voltrevo@gmail.com>
2023-10-26 16:27:21 +1100

    StorageOps

--- storage/src/storage.rs
+++ storage/src/storage.rs
@@ -1,19 +1,109 @@
 use std::{fmt::Debug as DebugTrait, rc::Rc};
 
-use rand::{rngs::ThreadRng, thread_rng};
+use rand::thread_rng;
 use serde::{Deserialize, Serialize};
 
 use crate::serde_rc::{deserialize_rc, serialize_rc};
 use crate::storage_key::StorageKey;
 
 pub struct Storage<SB: StorageBackend> {
-  rng: ThreadRng,
   sb: SB,
 }
 
 pub trait StorageBackendHandle<'a, E> {
   fn read(&self, key: StorageKey) -> Result<Option<Vec<u8>>, E>;
   fn write(&mut self, key: StorageKey, data: Option<Vec<u8>>) -> Result<(), E>;
+}
+
+pub trait StorageOps<E> {
+  fn read_entry(&mut self, key: StorageKey) -> Result<Option<StorageEntry>, E>;
+  fn write_entry(&mut self, key: StorageKey, data: Option<&StorageEntry>) -> Result<(), E>;
+
+  fn inc_ref(&mut self, key: StorageKey) -> Result<(), E>;
+  fn dec_ref(&mut self, key: StorageKey) -> Result<(), E>;
+
+  fn get_head(&mut self, name: &[u8]) -> Result<Option<StorageVal>, E>;
+  fn set_head(&mut self, name: &[u8], value: Option<&StorageVal>) -> Result<(), E>;
+}
+
+impl<'a, T, E> StorageOps<E> for T
+where
+  T: StorageBackendHandle<'a, E>,
+{
+  fn read_entry(&mut self, key: StorageKey) -> Result<Option<StorageEntry>, E> {
+    let data = match self.read(key)? {
+      Some(data) => data,
+      None => return Ok(None),
+    };
+
+    Ok(Some(bincode::deserialize(&data).unwrap()))
+  }
+
+  fn write_entry(&mut self, key: StorageKey, data: Option<&StorageEntry>) -> Result<(), E> {
+    self.write(key, data.map(|data| bincode::serialize(&data).unwrap()))
+  }
+
+  fn inc_ref(&mut self, key: StorageKey) -> Result<(), E> {
+    let mut entry = match self.read_entry(key)? {
+      Some(entry) => entry,
+      None => panic!("Key does not exist"),
+    };
+
+    entry.ref_count += 1;
+
+    self.write_entry(key, Some(&entry))
+  }
+
+  fn dec_ref(&mut self, key: StorageKey) -> Result<(), E> {
+    let mut entry = match self.read_entry(key)? {
+      Some(entry) => entry,
+      None => panic!("Key does not exist"),
+    };
+
+    entry.ref_count -= 1;
+
+    if entry.ref_count == 0 {
+      for key in entry.refs.iter() {
+        self.dec_ref(*key)?;
+      }
+
+      self.write(key, None)
+    } else {
+      self.write_entry(key, Some(&entry))
+    }
+  }
+
+  fn get_head(&mut self, name: &[u8]) -> Result<Option<StorageVal>, E> {
+    let key = match self.read(StorageKey::from_bytes(name))? {
+      Some(key_bytes) => StorageKey::from_bytes(&key_bytes),
+      None => return Ok(None),
+    };
+
+    let data = match self.read(key)? {
+      Some(data) => data,
+      None => panic!("Head points to non-existent key"),
+    };
+
+    Ok(Some(
+      bincode::deserialize::<StorageEntry>(&data)
+        .unwrap()
+        .deserialize(),
+    ))
+  }
+
+  fn set_head(&mut self, name: &[u8], value: Option<&StorageVal>) -> Result<(), E> {
+    let mut r = thread_rng();
+
+    let key = StorageKey::random(&mut r);
+
+    self.write(
+      key,
+      value.map(|value| bincode::serialize(&value.serialize()).unwrap()),
+    )?;
+
+    // TODO: Handle existing
+    self.write(StorageKey::from_bytes(name), Some(key.to_bytes()))
+  }
 }
 
 pub trait StorageBackend {
@@ -28,66 +118,15 @@ pub trait StorageBackend {
 
 impl<SB: StorageBackend> Storage<SB> {
   pub fn new(sb: SB) -> Self {
-    Self {
-      rng: thread_rng(),
-      sb,
-    }
-  }
-
-  pub fn read(&mut self, key: StorageKey) -> Result<Option<StorageEntry>, SB::Error<()>> {
-    self.sb.transaction(|sb| {
-      let data = match sb.read(key)? {
-        Some(data) => data,
-        None => return Ok(None),
-      };
-
-      Ok(Some(bincode::deserialize(&data).unwrap()))
-    })
-  }
-
-  pub fn write(&mut self, key: StorageKey, data: &StorageEntry) -> Result<(), SB::Error<()>> {
-    self.sb.transaction(|sb| {
-      let data = bincode::serialize(&data).unwrap();
-      sb.write(key, Some(data))
-    })
-  }
-
-  pub fn set_head(&mut self, name: &[u8], value: Option<&StorageVal>) -> Result<(), SB::Error<()>> {
-    let Self { sb, rng } = self;
-
-    sb.transaction(|sb| {
-      let mut r = rng.clone();
-
-      let key = StorageKey::random(&mut r);
-
-      sb.write(
-        key,
-        value.map(|value| bincode::serialize(&value.serialize()).unwrap()),
-      )?;
-
-      // TODO: Handle existing
-      sb.write(StorageKey::from_bytes(name), Some(key.to_bytes()))
-    })
+    Self { sb }
   }
 
   pub fn get_head(&mut self, name: &[u8]) -> Result<Option<StorageVal>, SB::Error<()>> {
-    self.sb.transaction(|sb| {
-      let key = match sb.read(StorageKey::from_bytes(name))? {
-        Some(key_bytes) => StorageKey::from_bytes(&key_bytes),
-        None => return Ok(None),
-      };
+    self.sb.transaction(|sb| sb.get_head(name))
+  }
 
-      let data = match sb.read(key)? {
-        Some(data) => data,
-        None => panic!("Head points to non-existent key"),
-      };
-
-      Ok(Some(
-        bincode::deserialize::<StorageEntry>(&data)
-          .unwrap()
-          .deserialize(),
-      ))
-    })
+  pub fn set_head(&mut self, name: &[u8], value: Option<&StorageVal>) -> Result<(), SB::Error<()>> {
+    self.sb.transaction(|sb| sb.set_head(name, value))
   }
 }
 
