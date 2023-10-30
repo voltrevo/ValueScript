a21191a1bac684e6451825a7afb884f0ec09807d
Andrew Morris <voltrevo@gmail.com>
2023-10-30 14:27:43 +1100

    Storable -> StorageEntity

--- storage/src/storage_entity.rs
+++ storage/src/storage_entity.rs
@@ -1,6 +1,6 @@
 use crate::{storage_entry::StorageEntry, storage_ops::StorageOps};
 
-pub trait Storable {
+pub trait StorageEntity {
   fn to_storage_entry<E, Tx: StorageOps<E>>(&self, tx: &mut Tx) -> StorageEntry;
   fn from_storage_entry<E, Tx: StorageOps<E>>(tx: &mut Tx, entry: StorageEntry) -> Self;
 }
