77882f0d0ad1d430716f397c95b283dfbaa4ff1f
Andrew Morris <voltrevo@gmail.com>
2024-07-24 15:58:30 +0900

    Add failing test

---
+++ inputs/failing/optimizerBug.ts
@@ -0,0 +1,17 @@
+// //! test_output(1)
+
+export default function main() {
+  return f([1]);
+}
+
+export function f(values: number[]): number {
+  const x = [
+    values[0],
+  ];
+
+  if (values[0]) {
+    return x[0];
+  }
+
+  return 0;
+}
